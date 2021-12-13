44ef52d5b5c13c1bf0d46117fff3651554bcbf4c
Dean Jones <84123704+deanhsjones@users.noreply.github.com>
2021-12-13 08:59:15 +0100

    Add files via upload
    Update: added player and player position marker
    added player position update
    todo: collision and door/key features

--- ArrayGame.cpp
+++ ArrayGame.cpp
@@ -2,11 +2,14 @@
 //
 
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
+const char kPlayerSymbol = '@';
 
 int GetIndexFromCoordinates(int X, int y, int width);
-void DrawLevel(char level[], int width, int height);
+void DrawLevel(char level[], int width, int height, int playerX, int playerY);
+void UpdatePlayerPosition(int& playerX, int& playerY);
 
 int main()
 {
@@ -29,7 +32,18 @@ int main()
                        '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'D', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', '|',
                        '+', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '+' };
 
-     DrawLevel(levelArray, kWidth, kHeight);
+    int playerX = 1;
+    int playerY = 1;
+
+    while (true)
+    {
+        system("cls");
+        DrawLevel(levelArray, kWidth, kHeight, playerX, playerY);
+        UpdatePlayerPosition(playerX, playerY);
+    }
+
+
+     DrawLevel(levelArray, kWidth, kHeight, playerX, playerY);
 }
 
 int GetIndexFromCoordinates(int x, int y, int width)
@@ -37,15 +51,59 @@ int GetIndexFromCoordinates(int x, int y, int width)
     return x + y * width;
 }
 
-void DrawLevel(char level[], int width, int height)
+void DrawLevel(char level[], int width, int height, int playerX, int playerY)
 {
     for (int y = 0; y < height; y++)
     {
         for (int x = 0; x < width; x++)
         {
+            if (playerX == x && playerY == y)
+            {
+                cout << kPlayerSymbol;
+            }
+            else
+            {
             int index = GetIndexFromCoordinates(x, y, width);
             cout << level[index];
+
+            }
         }
         cout << endl;
+    }
+}
+
+void UpdatePlayerPosition(int& playerX, int& playerY)
+{
+    char input = _getch();
+
+    switch (input)
+    {
+    case 'w':
+    case 'W':
+    {
+        playerY--;
+        break;
+    }
+    case 's':
+    case 'S':
+    {
+        playerY++;
+        break;
+    }
+    case 'a':
+    case 'A':
+    {
+        playerX--;
+        break;
+    }
+    case 'd':
+    case 'D':
+    {
+        playerX++;
+        break;
+    }
+    default:
+        break;
+
     }
 }
